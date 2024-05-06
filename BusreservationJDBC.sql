54bdaaf9bb8051d579c9e36cdeea88d2bd5a5240
Ponnulingam <168225396+Ponnulingam333@users.noreply.github.com>
2024-05-06 11:29:22 +0530

    Update BusreservationJDBC.sql

--- BusreservationJDBC.sql
+++ BusreservationJDBC.sql
@@ -1,7 +1,9 @@
 CREATE DATABASE busreservation;
+-- create database busreservation 
 
 USE busreservation;
 
+-- create table for storing passanger booking details
 CREATE TABLE booking(
 busno Int,
 bname VARCHAR(30),
@@ -12,48 +14,27 @@ bFrom VARCHAR(30),
 bTo VARCHAR(30),
 emailid VARCHAR(30) );
 
-drop table booking;
+-- create table bus for storing bus details
+CREATE TABLE bus(
+busno INT,
+capacity INT,
+ac BOOLEAN);
 
-create table bus(
-busno int,
-capacity int,
-ac boolean);
- Insert into bus values(1,1,true),(2,48,false),(3,50,true);
-select * from booking;
-delete from booking;
-select capacity from bus where busno=1;
-select * from bus;
-select count(bname) from booking ;
-drop table booking;
-select * from bus
-where exists (select capacity from bus where busno=1);
+-- insert the bus details in bus table
+INSERT INTO bus VALUES(1,1,true),(2,48,false),(3,50,true);
 
-select exists (select 2 from bus where busno=1) as mcheck; 
-select capacity from bus where busno=4;
+-- create table for storing district details
+CREATE TABLE District(
+sno INT,
+districtName VARCHAR(20));
 
-select exists(select 2920 from booking where busno=4);
-select bookingno from booking where busno=24;
-
-create table District(
-sno int,
-districtName varchar(20));
-
-insert into District values(1,"Ariyalur"),(2,"Chengalpattu"),(3,"Chennai"),(4,"Coimbatore"),(5,"Cuddalore"),(6,"Dharmapuri"),
+-- insert the district details in District table
+INSERT INTO District VALUES(1,"Ariyalur"),(2,"Chengalpattu"),(3,"Chennai"),(4,"Coimbatore"),(5,"Cuddalore"),(6,"Dharmapuri"),
 (7,"Dindigul"),(8,"Erode"),(9,"Kallakurichi"),(10,"Kancheepuram"),(11,"karur"),(12,"Krishnagiri"),(13,"Madurai"),
 (14,"Mayiladuthurai"),(15,"NagaPattinam"),(16,"Kanniyakumari"),(17,"Namakkal"),(18,"Perambalur"),(19,"Pudukottai"),
 (20,"Ramanathapuram"),(21,"Ranipet"),(22,"Salem"),(23,"Sivagangai"),(24,"Tenkasi"),(25,"Thanjavur"),(26,"Theni"),(27,"Thiruvallur"),
 (28,"Thiruvarur"),(29,"Thoothukudi"),(30,"Trichirappali"),(31,"Thirunelveli"),(32,"Tirupathur"),(33,"Tirupur"),
 (34,"Thiruvannamalai"),(35,"Nilgris"),(36,"Vellore"),(37,"Viluppuram"),(38,"Virudhunagar");
 
-select * from District;
-drop table district;
-select exists (select sno from District where districtName ="chennai");
-select * from booking;
-
-select exists (select busno from booking where bookingno=768);
-select busno from booking;
-
-select * from booking;
-show tables;
 
 
